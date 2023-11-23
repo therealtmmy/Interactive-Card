54b3775b37db49c1c22b1444720d5c7cd0839d73
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-11-23 09:16:46 +0100

    refactored codebase

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Components/Hero.css";
 import cardFront from "../interactive-card-details-form-main/interactive-card-details-form-main/images/bg-card-front.png";
 import cardBack from "../interactive-card-details-form-main/interactive-card-details-form-main/images/bg-card-back.png";
 
 const Hero = () => {
+  const [number, setNumber] = useState("");
+
   return (
     <div className="Hero">
       <div>
-        <div>
+        <div className="frontCard">
           <img className="cardFront" src={cardFront} alt="Card Front" />
+          <p className="number">0000 0000 0000 0000</p>
+          <span className="cardSpan">
+            <p>JANE APPLESEED</p>
+            <p>00/00</p>
+          </span>
+          {/* <table>
+            <tr>
+              <td>{name}</td>
+              <td>{name}</td>
+            </tr>
+          </table> */}
         </div>
 
-        <div>
+        <div className="backCard">
           <img className="cardBack" src={cardBack} alt="Card Back" />
+          <p className="CVC">000</p>
         </div>
       </div>
 
       <div className="inputField">
         <label htmlFor="">CARDHOLDER NAME</label>
-        <input type="text" placeholder="e.g. Jane Appleseed" />
+        <input
+          type="text"
+          placeholder="e.g. Jane Appleseed"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
 
         <label htmlFor="">CARD NUMBER</label>
         <input type="text" placeholder="e.g. 1234 5678 9123 0000" />
